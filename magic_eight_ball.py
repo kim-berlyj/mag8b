500ceec28b1ea42828d6c39315c33d86b18d8855
Kimberly Jones <KimberlyJones@Kimberlys-MacBook-Pro.local>
2015-06-24 20:32:05 -0700

    import questions

--- magic_eight_ball.py
+++ magic_eight_ball.py
@@ -1,3 +1,21 @@
 # prompt user to ask question
-# return random answer to user
+
+#randomly answer question from response file
 # ask user if they have another question or if they would like to exit
+
+# return random answer to user
+my_file=open("response.rtf")
+responses=my_file.readlines()
+my_file.close()
+
+import random 
+
+def response(question):
+	print question1=raw_input("Would you like to play?")
+	if True:
+		question2=raw_input("What is your question?")
+		random.choice(responses)
+		question3=raw_input("Do you have another question?")
+	else:
+		return "Goodbye"
+
