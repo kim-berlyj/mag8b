623e5f54ea8f7e447f34367d1108fe38d039499e
Kimberly Jones <KimberlyJones@Kimberlys-MacBook-Pro.local>
2015-06-29 20:13:11 -0700

    completed program exit with Goodbye

--- magic_eight_ball.py
+++ magic_eight_ball.py
@@ -7,23 +7,23 @@
 my_file=open("response.rtf")
 responses=my_file.readlines()
 my_file.close()
-# responses2=responses.strip()
+# responses2=responses.strip("\n")
+import random
+question=raw_input("Do you have a question for me? ")
 
-import random 
-
-question1=raw_input("Would you like to play? ")
-
-def response(question):
-	while(True):
-		question2=raw_input("What is your question? ")
+def response():
+	while (question=="yes"):
+		raw_input("What is your question? ")
 		print random.choice(responses)
-		if (raw_input("Do you have another question? ")=="yes"):
-			response(question)
-		else:
+		if (raw_input("Do you have another question? ")=="no"):
 			print "Goodbye"
+			return
+		else:
+			response()
 
-response(question1)
+response()
 
 # need to figure out how to exit program with out asking "What is your question?" after printing "Goodbye"
+# and how to print response without / after answer
 
 
