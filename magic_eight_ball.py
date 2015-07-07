2e360ce3d6a4ce57ec85851f1beb2697aea7aa06
Kimberly Jones <KimberlyJones@Kimberlys-MacBook-Pro.local>
2015-07-06 21:11:25 -0700

    finalized with 8 ball graphic

--- magic_eight_ball.py
+++ magic_eight_ball.py
@@ -1,32 +1,30 @@
-# prompt user to ask question
-
-#randomly answer question from response file
-# ask user if they have another question or if they would like to exit
-
-# return random answer to user
 my_file=open("response.txt")
 responses=my_file.readlines()
 my_file.close()
 import random
-question=raw_input("Do you have a question for me? ")
+question=raw_input("Do you have a question for me? ").lower()
+
+
 
 def response():
-	while (question=="yes" or question=="Yes"):
+	while (question=="yes"):
 		raw_input("What is your question? ")
+		print "       /\\"
+		print "      /  \\"
+		print "     /    \\"
+		print "    /      \\"
 		print random.choice(responses)
-		question1=raw_input("Do you have another question? ")
-		if (question1=="no" or question1=="No"):
+		print " /            \\"
+		print "/              \\"
+		print " ---------------"
+		question1=raw_input("Do you have another question? ").lower()
+		if (question1=="no"):
 			print "Goodbye"
 			exit()
-		elif (question1=="yes" or question1=="Yes"):
+		elif (question1=="yes"):
 			response()
 		else:
 			print "Goodbye"
 			exit()
 
 response()
-
-# need to figure out how to exit program with out asking "What is your question?" after printing "Goodbye"
-# and how to print response without / after answer
-
-
